40a7070296be3002456aba4574bc91c8e10e82a3
amritam03 <amritam.web@gmail.com>
2026-05-24 00:03:49 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,14 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import os
 
-BOT_TOKEN = os.getenv("8983135704:AAEPecaHWbRY7Rej3fKaC2_JeeKYZPAzQHw")
-CHAT_ID = os.getenv("982948994")
+BOT_TOKEN=os.getenv("BOT_TOKEN")
+CHAT_ID=os.getenv("CHAT_ID")
 
 URL="https://www.karzanddolls.com/search?search=mini%20gt"
-
-seen=set()
 
 def send(msg):
     requests.post(
@@ -19,42 +16,29 @@ def send(msg):
         }
     )
 
-def check_stock():
+headers={
+    "User-Agent":"Mozilla/5.0"
+}
 
-    headers={
-      "User-Agent":"Mozilla/5.0"
-    }
+r=requests.get(URL,headers=headers)
 
-    r=requests.get(URL,headers=headers)
+soup=BeautifulSoup(
+    r.text,
+    "html.parser"
+)
 
-    soup=BeautifulSoup(
-        r.text,
-        "html.parser"
-    )
+for a in soup.find_all("a"):
 
-    for a in soup.find_all("a"):
+    text=a.get_text(strip=True)
 
-        text=a.get_text(strip=True)
-        href=a.get("href")
+    href=a.get("href")
 
-        if "MINI GT" in text.upper():
+    if "MINI GT" in text.upper():
 
-            if text not in seen:
+        if href:
 
-                seen.add(text)
+            send(
+f"🔥 Mini GT listing found\nhttps://www.karzanddolls.com{href}"
+            )
 
-                if href:
-
-                    send(
-f"🔥 Mini GT found!\n\n{text}\nhttps://www.karzanddolls.com{href}"
-                    )
-
-while True:
-
-    try:
-        check_stock()
-
-    except Exception as e:
-        print(e)
-
-    time.sleep(30)
+            break
