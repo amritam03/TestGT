aea3d15850d7c8b8b52b164aef64ef20e7e63e7a
amritam03 <amritam.web@gmail.com>
2026-05-24 01:10:52 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -5,7 +5,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-URL = "https://www.karzanddolls.com/details/tsm%2Bmodel%2Bcars/mini-gt/MTY1"
+URL = "https://www.karzanddolls.com/details/mini+gt+/mini-gt/MTY1"
 
 headers = {
     "User-Agent":"Mozilla/5.0"
