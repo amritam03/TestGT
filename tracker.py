edcfeb7bfb068a6be1791b24999465ae7f3e9e13
amritam03 <amritam.web@gmail.com>
2026-05-24 00:20:54 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,46 +1,21 @@
 import requests
-from bs4 import BeautifulSoup
 import os
 
-BOT_TOKEN = os.getenv("8983135704:AAEPecaHWbRY7Rej3fKaC2_JeeKYZPAzQHw")
-CHAT_ID = os.getenv("982948994")
+BOT_TOKEN = os.getenv(8983135704:AAEPecaHWbRY7Rej3fKaC2_JeeKYZPAzQHw)
+CHAT_ID = os.getenv(982948994)
 
-URL = "https://www.karzanddolls.com/search?search=mini%20gt"
+print("BOT:", BOT_TOKEN[:10] if BOT_TOKEN else "missing")
+print("CHAT:", CHAT_ID)
 
-def send(msg):
-    r = requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        data={
-            "chat_id": CHAT_ID,
-            "text": msg
-        }
-    )
-    print("Telegram:", r.status_code)
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+r = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ GitHub Actions test message"
+    }
+)
 
-print("Checking website...")
-
-r = requests.get(URL, headers=headers)
-
-print("Website status:", r.status_code)
-
-soup = BeautifulSoup(r.text, "html.parser")
-
-found = False
-
-for text in soup.stripped_strings:
-
-    if "MINI GT" in text.upper():
-
-        found = True
-        print("Found:", text)
-
-        send(f"🔥 Mini GT detected:\n\n{text}")
-
-        break
-
-if not found:
-    print("No Mini GT found")
+print("Status:", r.status_code)
+print("Response:", r.text)
