f8f9ba8517a9e74a98c954c4077399f1aa3cc803
amritam03 <amritam.web@gmail.com>
2026-05-24 00:23:07 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-BOT_TOKEN = os.getenv(8983135704:AAEPecaHWbRY7Rej3fKaC2_JeeKYZPAzQHw)
-CHAT_ID = os.getenv(982948994)
+BOT_TOKEN = os.getenv("8983135704:AAEPecaHWbRY7Rej3fKaC2_JeeKYZPAzQHw")
+CHAT_ID = os.getenv("982948994")
 
 print("BOT:", BOT_TOKEN[:10] if BOT_TOKEN else "missing")
 print("CHAT:", CHAT_ID)
